a7baa59ea8064267c6184927ee2bd622b3cb6cef
Holger Trampe <htrampe@gmail.com>
2023-04-27 14:27:22 +0200

    Meilensteine und Ziele aktualisiert

--- src/main/java/gymhum/memory/MemoryApplication.java
+++ src/main/java/gymhum/memory/MemoryApplication.java
@@ -9,5 +9,37 @@ public class MemoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MemoryApplication.class, args);
 	}
-
 }
+
+/*
+
+
+	Projektplanung
+
+	Ziele:
+		1. Zwei Spieler spielen gegenseitig ein Memory-Spiel mit 16x16 Feldern.
+		2. Die Spieler werden vorher ausgewählt und in einer Partie sortiert.
+		3. Die Partien werden in einer SQL-Datenbank gespeichert.
+		4. Jeder Spieler hat eine Statistik, bei der alle Spiele dargestellt werden und ersichtlich wird,
+		   wie viel er gewonnen/verloren hat.
+
+	Meileinsteine
+	- Grundprojekt aufsetzen (OK)
+	- Framework Material CSS https://materializecss.com/ einbinden inkl. Navigation für Memory und Historie sowie Spieler anlegen/löschen/verändern und Partien löschen ()
+	- Spielmodel implementieren ()
+	- Partiemodel implementieren ()
+	- Memoryansicht implementieren ()
+		- Bilder/Zeichen raussuchen und einpflegen ()
+		- Zufallsplatzierung ()
+	- Spiellogik aufbauen ()
+	- Gewinn/Verloren-Logik implementieren ()
+	- DB anlegen und Tabellenstruktur aufbauen entsprechend der Models ()
+		- Tabelle für Spieler ()
+		- Tabelle für Partien ()
+		- Fremdschlüssel Partie-Spieler ()
+	- Spieler und Partie in DB speichern ()
+	- Historienansicht aufbauen ()
+
+	- Test und Bugfixing die ganze Zeit beibehalten
+
+ */
